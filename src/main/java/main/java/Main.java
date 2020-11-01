0a76cef921713c485a87a6fb74335cfcf6b2938a
ilsave <bulldoo@yandex.ru>
2020-11-01 14:36:32 +0300

    hey

---
+++ src/main/java/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+import org.apache.commons.lang3.StringUtils;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.reverse("Hello"));
+        System.out.println("Hello");
+       // System.out.println("hello".isBlank());
+    }
+}
